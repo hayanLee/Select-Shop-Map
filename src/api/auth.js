58064789efa3778100c7778043d69dc55f00efc7
hayanLee <hayanlee.dev@gmail.com>
2024-06-18 17:00:51 +0900

    update : 회원가입 시 db 저장
    - 회원가입 시 에러처리

--- src/api/auth.js
+++ src/api/auth.js
@@ -1,12 +1,23 @@
 import supabase from '../supabase/supabaseClient';
 
-export const signUpWithEmail = async ({ email, password }) => {
+export const signUpWithEmail = async ({ email, password, nickname }) => {
   const { data, error } = await supabase.auth.signUp({
     email,
     password
   });
-  if (data.user) alert('회원가입 성공하였습니다!');
-  if (error) alert('이미 존재하는 이메일 또는 비밀번호는 6자리 이상으로 해주세요');
+
+  if (error) {
+    if (error.code === 'weak_password') alert('비밀번호는 6자리 이상이여야 합니다.');
+    else alert('회원가입 중 오류가 발생했습니다.');
+    return;
+  }
+
+  if (data.user) {
+    const { id, email } = data.user;
+    if (!nickname) nickname = String(crypto.randomUUID().slice(1, 8));
+    await insertUserData({ id, email, nickname });
+    alert('회원가입 성공하였습니다!');
+  }
 };
 
 export const LoginWithEmail = async ({ email, password }) => {
@@ -33,4 +44,16 @@ export const getUser = async () => {
   if (error) {
     console.log('로그인한 유저 없음', error);
   }
+};
+
+export const insertUserData = async (userInfo) => {
+  console.log(userInfo);
+  const { id, email, nickname } = userInfo;
+  const { data, error } = await supabase.from('users').insert({
+    id,
+    created_at: new Date(),
+    email,
+    nickname
+  });
+  if (error) alert('회원 정보 저장 중 오류가 발생했습니다.');
 };
