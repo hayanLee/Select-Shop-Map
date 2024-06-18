47670de2d1f3a6c680a122618e1c190500fc9728
sseul <hanbat107@naver.com>
2024-06-18 18:14:32 +0900

    style : DetailPage 레이아웃 수정, 박스별 쉐도우 효과

--- src/pages/DetailPage.jsx/DetailPage.jsx
+++ src/pages/DetailPage.jsx/DetailPage.jsx
@@ -4,13 +4,13 @@ const DetailPage = () => {
   return (
     <div className="flex min-h-screen justify-center">
       <main className="w-full max-w-[1320px] p-4">
-        <div className="flex space-x-6 rounded-lg bg-hover p-[48px_36px_30px_36px] shadow-lg">
+        <div className="mt-[20px] flex space-x-6 rounded-lg bg-hover p-[48px_36px_30px_36px] shadow-lg">
           <img
             // src={require('../assets/image.jpg')} // 실제 이미지 경로로 변경
             alt="Store"
-            className="h-64 w-1/2 rounded-lg bg-white object-cover"
+            className="h-64 w-1/2 rounded-lg bg-white object-cover shadow-md"
           />
-          <div className="relative w-1/2 rounded-lg bg-white p-6">
+          <div className="relative w-1/2 rounded-lg bg-white p-6 shadow-md">
             <h1 className="text-3xl font-bold">Title</h1>
             <p className="mt-2 text-gray-700">위치</p>
             <p className="mt-2 text-gray-700">위치</p>
@@ -19,7 +19,7 @@ const DetailPage = () => {
           </div>
         </div>
 
-        <div className="mt-6 rounded-lg bg-hover p-[48px_36px_30px_36px]">
+        <div className="mt-9 rounded-lg bg-hover p-[48px_36px_30px_36px] shadow-md">
           <section className="rounded-lg bg-white p-4 shadow-md">
             <div className="flex items-center space-x-[22px]">
               <textarea placeholder="리뷰를 남겨주세요" className="h-[70px] w-full rounded-lg border p-2" />
