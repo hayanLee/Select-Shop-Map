4b1a57686fd909efe37b5a93f9a40b1d6745c417
sseul <hanbat107@naver.com>
2024-06-19 18:27:40 +0900

    style: header 폰트 및 레이아웃 스타일 수정

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -8,8 +8,8 @@ function Header() {
   const [user, setUser] = useState(null);
   const navigate = useNavigate();
 
-  //localStorage에서 키 값을 가져와 사용자 정보 확인 후 user 상태 업데이트
-   useEffect(() => {
+  // localStorage에서 키 값을 가져와 사용자 정보 확인 후 user 상태 업데이트
+  useEffect(() => {
     const checkUser = () => {
       const currentUser = JSON.parse(localStorage.getItem('sb-qqfwyfugvnciounpkmfi-auth-token'));
       if (currentUser && currentUser.user) {
@@ -39,36 +39,38 @@ function Header() {
   };
 
   return (
-    <div className="flex h-20 w-full items-center justify-between bg-main px-96">
-      <div className="flex items-center">
-        <Link to={'/'}>
-          <img src="public/favicon.png" alt="logo" className="mr-3" style={{ height: '75px' }} />
-        </Link>
-        <p className="text-3xl font-bold text-blue-950">칠리칠리 소품랜드</p>
-      </div>
-      <div className="flex items-center">
-        <div className="relative">
-          <FaMagnifyingGlass className="absolute left-3 top-1/2 -translate-y-1/2 transform text-blue-950" />
-          <input
-            placeholder="지역을 검색해 주세요..."
-            className="h-10 w-80 rounded-3xl pl-10 pr-4 focus:outline-active"
-          />
-        </div>
-        {user ? (
-          <div className="flex items-center">
-            <Link to="/mypage" className="ml-4 text-blue-950">
-              <p className="text-xl font-bold">My Page</p>
-            </Link>
-            <p className="ml-4 cursor-pointer text-xl font-bold text-blue-950" onClick={handleLogout}>
-              Log Out
-            </p>
-          </div>
-        ) : (
-          <Link to="/login" className="ml-4 flex items-center text-blue-950">
-            <LuArrowRightToLine className="h-28 text-3xl" />
-            <p className="ml-3 text-xl font-bold">Log In</p>
+    <div className="bg-main">
+      <div className="mx-auto flex h-16 w-full max-w-screen-xl items-center justify-between px-4 md:px-12">
+        <div className="flex items-center">
+          <Link to={'/'}>
+            <img src="public/favicon.png" alt="logo" className="mr-2" style={{ height: '50px' }} />
           </Link>
-        )}
+          <p className="text-xl font-bold text-blue-950">칠리칠리 소품랜드</p>
+        </div>
+        <div className="flex items-center">
+          <div className="relative">
+            <FaMagnifyingGlass className="absolute left-3 top-1/2 -translate-y-1/2 transform text-blue-950" />
+            <input
+              placeholder="지역을 검색해 주세요..."
+              className="h-8 w-64 rounded-3xl pl-10 pr-4 focus:outline-active"
+            />
+          </div>
+          {user ? (
+            <div className="flex items-center">
+              <Link to="/mypage" className="ml-3 text-blue-950">
+                <p className="text-lg font-bold">My Page</p>
+              </Link>
+              <p className="ml-3 cursor-pointer text-lg font-bold text-blue-950" onClick={handleLogout}>
+                Log Out
+              </p>
+            </div>
+          ) : (
+            <Link to="/login" className="ml-3 flex items-center text-blue-950">
+              <LuArrowRightToLine className="h-5 w-5 text-2xl" />
+              <p className="ml-3 text-lg font-bold">Log In</p>
+            </Link>
+          )}
+        </div>
       </div>
     </div>
   );
