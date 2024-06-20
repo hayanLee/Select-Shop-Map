d8c2bc4dba4dd6802e345b4df5245e609e23942e
suzy <tnwl9540@gmail.com>
2024-06-20 09:55:29 +0900

    add : 스켈레톤 UI 파일 추가

---
+++ src/components/KakaoMap/KakaoMapLoading.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ContentLoader from 'react-content-loader';
+
+const KakaoMapLoading = () => (
+  <ContentLoader viewBox="0 0 400 480">
+    <rect x="0" y="0" rx="2" ry="2" width="400" height="480" />
+  </ContentLoader>
+);
+
+export default KakaoMapLoading;
