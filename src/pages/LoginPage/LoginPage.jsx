cf8327d391f95dce7d40cd383ecbc37743fd3c06
eldoradodo <rla1eh2dus3@naver.com>
2024-06-20 09:35:21 +0900

    fix : 로그인 페이지 dev 코드 테스트

--- src/pages/LoginPage/LoginPage.jsx
+++ src/pages/LoginPage/LoginPage.jsx
@@ -1,97 +1,67 @@
-import { useEffect, useState } from 'react';
-import { loginWithEmail } from '../../api/auth';
+import { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
+import { loginWithEmail } from '../../api/auth';
+import { validateForm } from '../../utils/validateForm';
 
 function LoginPage() {
-const [email, setEmail] = useState('');
-const [password, setPassword] = useState('');
-const navigate = useNavigate();
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const navigate = useNavigate();
 
-useEffect(() => {
-localStorage.removeItem('savedEmail');
-localStorage.removeItem('savedPassword');
-}, []);
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    if (!validateForm({ email, password })) return;
 
-useEffect(() => {
-const savedEmail = localStorage.getItem('savedEmail');
-const savedPassword = localStorage.getItem('savedPassword');
-if (savedEmail) setEmail(savedEmail);
-if (savedPassword) setPassword(savedPassword);
-}, []);
+    try {
+      const userInfo = await loginWithEmail({ email, password });
+      if (userInfo) {
+        localStorage.setItem('userInfo', JSON.stringify(userInfo)); // 로컬스토리지에 저장
 
-const validateForm = () => {
-if (!email || !password) {
-alert('이메일과 비밀번호를 입력하세요.');
-return false;
-}
-const emailRegex = /^[^\s@]+@[^\s@]+.[^\s@]+$/;
-if (!emailRegex.test(email)) {
-alert('올바른 이메일 형식이 아닙니다.');
-return false;
-}
-if (password.length < 6) {
-alert('비밀번호는 6자리 이상이어야 합니다.');
-return false;
-}
-return true;
-};
+        setEmail('');
+        setPassword('');
+        navigate('/');
+      } else {
+        console.error('로그인 후 사용자 정보를 받아오지 못했습니다.');
+      }
+    } catch (error) {
+      console.error('로그인 실패:', error.message);
+    }
+  };
 
-const handleLogin = async (e) => {
-e.preventDefault();
-if (!validateForm()) return;
-try {
-const userInfo = await loginWithEmail({ email, password });
-if (userInfo) {
-localStorage.setItem('userInfo', JSON.stringify(userInfo));
-localStorage.setItem('sb-qqfwyfugvnciounpkmfi-auth-token', JSON.stringify({ user: userInfo }));
-window.dispatchEvent(new Event('storage'));
-navigate('/');
-setEmail('');
-setPassword('');
-} else {
-console.error('로그인 후 사용자 정보를 받아오지 못했습니다.');
-}
-} catch (error) {
-console.error('로그인 실패:', error.message);
-}
-};
-
-return (
-<div className="mt-32 flex justify-center px-28">
-<div className="flex w-full flex-col">
-<p className="mb-9 text-4xl font-bold">Log In</p>
-<form className="flex flex-col gap-5" onSubmit={handleLogin}>
-<div>
-<p>Email Address</p>
-<input
-type="email"
-value={email}
-onChange={(e) => setEmail(e.target.value)}
-className="h-12 w-full bg-input pl-4 focus
-shadow-lg"
-placeholder="이메일을 입력하세요."
-/>
-</div>
-<div>
-<p>Password</p>
-<input
-type="password"
-value={password}
-onChange={(e) => setPassword(e.target.value)}
-className="h-12 w-full bg-input pl-4 focus
-shadow-lg"
-placeholder="비밀번호를 입력하세요."
-/>
-<p className="mt-1 text-xs">비밀 번호는 6자리 이상이어야 합니다.</p>
-</div>
-<button className="mt-3 h-12 w-full bg-active pl-4 font-bold text-white hover:bg-hover">Log In</button>
-</form>
-<Link to="/signup" className="mt-3 text-[#3490dc]">
-아직 계정이 없으신가요? 회원 가입 하러가기
-</Link>
-</div>
-</div>
-);
+  return (
+    <div className="mt-32 flex justify-center px-28">
+      <div className="flex w-full flex-col">
+        <p className="mb-9 text-4xl font-bold">Log In</p>
+        <form className="flex flex-col gap-5" onSubmit={handleLogin}>
+          <div>
+            <p>Email Address</p>
+            <input
+              type="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              className="h-12 w-full bg-input pl-4 shadow-lg focus:outline-active"
+              placeholder="이메일을 입력하세요."
+            />
+          </div>
+          <div>
+            <p>Password</p>
+            <input
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              className="h-12 w-full bg-input pl-4 shadow-lg focus:outline-active"
+              placeholder="비밀번호를 입력하세요."
+            />
+            <p className="mt-1 text-xs">비밀 번호는 6자리 이상이어야 합니다.</p>
+          </div>
+          <button className="mt-3 h-12 w-full bg-active pl-4 font-bold text-white hover:bg-hover">Log In</button>
+        </form>
+        <Link to="/signup" className="mt-3 text-[#3490dc]">
+          아직 계정이 없으신가요? 회원 가입 하러가기
+        </Link>
+      </div>
+    </div>
+  );
 }
 
 export default LoginPage;
