2725ea12fa0643e773429d78247487ebe9c017b7
hayanLee <hayanlee.dev@gmail.com>
2024-06-21 01:35:32 +0900

    fix : 좋아요, 리뷰 에러 수정 + 안쓰는 코드 삭제

--- src/pages/MyPage/MyPage.jsx
+++ src/pages/MyPage/MyPage.jsx
@@ -1,60 +1,23 @@
-import React, { useState, useEffect, useContext } from 'react';
-import SavedShopsList from './SavedShopsList.jsx';
+import React from 'react';
 import ReviewsList from './ReviewsList.jsx';
-import { getUserInfo } from '../../api/auth';
-import { AuthContext } from '../../contexts/AuthContext';
+import SavedShopsList from './SavedShopsList.jsx';
 
 const MyPage = () => {
-  const { userInfo } = useContext(AuthContext);
-  const [nickname, setNickname] = useState('');
-  const [userId, setUserId] = useState('');
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    const fetchNickname = async () => {
-      try {
-        if (userInfo) {
-          const userId = userInfo.id;
-          const user = await getUserInfo(userId);
-
-          if (user) {
-            setNickname(user.nickname);
-            setUserId(userId);
-          } else {
-            console.error('유저 정보를 불러오는데 실패했습니다.');
-          }
-        }
-      } catch (error) {
-        console.error('닉네임을 불러오는데에 실패했습니다.', error.message);
-      } finally {
-        setLoading(false);
-      }
-    };
-
-    fetchNickname();
-  }, [userInfo]);
-
-  if (loading) {
-    return <div>Loading...</div>;
-  }
+  const { id: userId, nickname } = JSON.parse(localStorage.getItem('userInfo'));
 
   return (
-    <div className="w-full min-h-screen flex flex-col items-center">
-      <div className="container bg-[#E5E1EF] p-4 rounded-lg shadow mt-16 mb-20 w-3/4">
-        <h1 className="text-3xl font-bold mb-10">💜{nickname || 'Loading...'}님의 페이지💜</h1>
+    <div className="flex min-h-screen w-full flex-col items-center">
+      <div className="container mb-20 mt-16 w-3/4 rounded-lg bg-[#E5E1EF] p-4 shadow">
+        <h1 className="mb-10 text-3xl font-bold">💜{nickname || 'Loading...'}님의 페이지💜</h1>
         <div className="my-4">
-          <h2 className="text-xl font-semibold mb-4">
-            💜{nickname || 'Loading...'}💜님의 찜 목록
-          </h2>
-          <div className="p-4 bg-gray-100 rounded-lg shadow min-h-40 mb-8">
+          <h2 className="mb-4 text-xl font-semibold">💜{nickname || 'Loading...'}💜님의 찜 목록</h2>
+          <div className="mb-8 min-h-40 rounded-lg bg-gray-100 p-4 shadow">
             <SavedShopsList userId={userId} />
           </div>
         </div>
         <div className="my-4">
-          <h2 className="text-xl font-semibold mb-4">
-            💜{nickname || 'Loading...'}💜님이 작성한 리뷰
-          </h2>
-          <div className="p-4 bg-gray-100 rounded-lg shadow min-h-40 mb-4">
+          <h2 className="mb-4 text-xl font-semibold">💜{nickname || 'Loading...'}💜님이 작성한 리뷰</h2>
+          <div className="mb-4 min-h-40 rounded-lg bg-gray-100 p-4 shadow">
             <ReviewsList userId={userId} />
           </div>
         </div>
