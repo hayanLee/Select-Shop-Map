43352d10065c1d2b94cdfa812e2793f5adfb6cf5
sseul <hanbat107@naver.com>
2024-06-20 16:32:44 +0900

    update : validateForm.js에서 alert창 sweetalert2 추가

--- src/utils/validateForm.js
+++ src/utils/validateForm.js
@@ -1,19 +1,41 @@
+import Swal from 'sweetalert2';
+
 export const validateForm = ({ email, password, nickname = null }) => {
   if (nickname !== null && nickname.trim() === '') {
-    alert('닉네임을 입력하세요.');
+    Swal.fire({
+      title: '오류',
+      text: '닉네임을 입력하세요.',
+      icon: 'error',
+      confirmButtonText: '확인'
+    });
     return false;
   }
   if (!email || !password) {
-    alert('이메일과 비밀번호를 입력하세요.');
+    Swal.fire({
+      title: '오류',
+      text: '이메일과 비밀번호를 입력하세요.',
+      icon: 'error',
+      confirmButtonText: '확인'
+    });
     return false;
   }
   const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
   if (!emailRegex.test(email)) {
-    alert('올바른 이메일 형식이 아닙니다.');
+    Swal.fire({
+      title: '오류',
+      text: '올바른 이메일 형식이 아닙니다.',
+      icon: 'error',
+      confirmButtonText: '확인'
+    });
     return false;
   }
   if (password.length < 6) {
-    alert('비밀번호는 6자리 이상이어야 합니다.');
+    Swal.fire({
+      title: '오류',
+      text: '비밀번호는 6자리 이상이어야 합니다.',
+      icon: 'error',
+      confirmButtonText: '확인'
+    });
     return false;
   }
 
