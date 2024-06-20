6ee57d1a0ba6490e1bd267efda4f7bba1c89bf57
eldoradodo <rla1eh2dus3@naver.com>
2024-06-20 17:57:35 +0900

    fix : 처음에 새로고침 없이 등록 버튼이 눌리지 않는 에러 해결

--- src/components/PrivateRoute.jsx
+++ src/components/PrivateRoute.jsx
@@ -1,26 +1,16 @@
-import React, { useContext, useEffect } from 'react';
-import { Navigate, useLocation, useNavigate } from 'react-router-dom';
+import React from 'react';
+import { Navigate } from 'react-router-dom';
+import { useContext } from 'react';
 import { AuthContext } from '../contexts/AuthContext';
-import Swal from 'sweetalert2';
 
 const PrivateRoute = ({ children }) => {
   const { isAuthenticated } = useContext(AuthContext);
-  const location = useLocation();
-  const navigate = useNavigate();
 
-  useEffect(() => {
-    if (!isAuthenticated) {
-      Swal.fire('로그인을 먼저 해주세요!', '', 'warning').then(() => {
-        navigate('/login', { replace: true });
-      });
-    }
-  }, [isAuthenticated, navigate]);
-
-  if (isAuthenticated) {
-    return children;
+  if (!isAuthenticated) {
+    return <Navigate to="/login" />;
   }
 
-  return null;
+  return children;
 };
 
 export default PrivateRoute;
