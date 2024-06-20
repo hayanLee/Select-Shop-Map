a5307075a3fca317f4808c93fe4fbd3be1c695ee
hayanLee <hayanlee.dev@gmail.com>
2024-06-20 22:32:58 +0900

    style : placeCard 스타일링
    - 안쓰는 컴포넌트 제거
    - vercel.json 설정

--- src/routes/router.jsx
+++ src/routes/router.jsx
@@ -1,11 +1,11 @@
 import { createBrowserRouter } from 'react-router-dom';
+import PrivateRoute from '../components/PrivateRoute';
 import DefaultLayout from '../layouts/DefaultLayout';
 import DetailPage from '../pages/DetailPage.jsx/DetailPage';
 import HomePage from '../pages/HomePage';
 import LoginPage from '../pages/LoginPage';
 import MyPage from '../pages/MyPage/MyPage.jsx';
 import SignUpPage from '../pages/SignUpPage';
-import PrivateRoute from '../components/PrivateRoute';
 
 const router = createBrowserRouter([
   {
@@ -13,7 +13,14 @@ const router = createBrowserRouter([
     children: [
       { path: '/', element: <HomePage /> },
       { path: '/shop/:shopId', element: <DetailPage /> },
-      { path: '/myPage', element: <PrivateRoute><MyPage /></PrivateRoute> },
+      {
+        path: '/myPage',
+        element: (
+          <PrivateRoute>
+            <MyPage />
+          </PrivateRoute>
+        )
+      },
       { path: '/login', element: <LoginPage /> },
       { path: '/signUp', element: <SignUpPage /> }
     ]
