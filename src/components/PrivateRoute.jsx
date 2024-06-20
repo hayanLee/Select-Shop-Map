858be46f255dd3e7fa5c5cdea4b0cabef784e2a4
eldoradodo <rla1eh2dus3@naver.com>
2024-06-20 18:08:39 +0900

    fix : 로그인 정보가 없는 상황에서 mypage 접근시 로그인 페이지로 이동 시키기

--- src/components/PrivateRoute.jsx
+++ src/components/PrivateRoute.jsx
@@ -1,16 +1,25 @@
-import React from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { Navigate } from 'react-router-dom';
-import { useContext } from 'react';
 import { AuthContext } from '../contexts/AuthContext';
+import Swal from 'sweetalert2';
 
 const PrivateRoute = ({ children }) => {
   const { isAuthenticated } = useContext(AuthContext);
+  const [showAlert, setShowAlert] = useState(false);
 
-  if (!isAuthenticated) {
+  useEffect(() => {
+    if (!isAuthenticated) {
+      Swal.fire('로그인되지 않은 사용자입니다!', '로그인부터 해주세요', 'warning').then(() => {
+        setShowAlert(true);
+      });
+    }
+  }, [isAuthenticated]);
+
+  if (showAlert) {
     return <Navigate to="/login" />;
   }
 
-  return children;
+  return isAuthenticated ? children : null;
 };
 
 export default PrivateRoute;
